759f9aedb23f21242acf3661f2d4f86878b97477
Hanel Tonna <crackdark15@gmail.com>
2019-07-17 11:37:30 +0700

    shopping list cell can be deleted

---
+++ Mix & Plan/ShopList/deletecellprotocol.swift
@@ -0,0 +1,12 @@
+//
+//  deletecellprotocol.swift
+//  Mix & Plan
+//
+//  Created by Hansel Tonna on 17/07/19.
+//  Copyright © 2019 aurelia  natasha. All rights reserved.
+//
+
+import Foundation
+protocol deletecell {
+    func deletecellfunc(row:Int)  
+}
